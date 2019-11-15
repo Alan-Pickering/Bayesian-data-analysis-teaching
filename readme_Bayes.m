d3b95787502ba54f1fbde5db0580c535d054efcd
Alan-Pickering <57485507+Alan-Pickering@users.noreply.github.com>
2019-11-15 16:22:47 +0000

    Add files via upload

---
+++ readme_Bayes.m
@@ -0,0 +1,12 @@
+%readme_Bayes.m
+%
+%To use AP's Bayesinan modelling code:-
+%1) Copy these files to a folder.
+%
+%bayes_table.m
+%more will be added here later
+%
+%2) Run bayes_table.m to explore a classic problem in Bayesian estimation
+%3) For more info type 
+%   help bayes_table 
+%   at the Matlab command prompt
