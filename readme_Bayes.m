01e9046d6a716e1e562c74a42a03755dd0e3a48e
Alan-Pickering <57485507+Alan-Pickering@users.noreply.github.com>
2019-11-15 17:53:02 +0000

    Add files via upload

--- readme_Bayes.m
+++ readme_Bayes.m
@@ -1,12 +1,23 @@
 %readme_Bayes.m
 %
-%To use AP's Bayesinan modelling code:-
-%1) Copy these files to a folder.
+%To use AP's Bayesian modelling code:-
+%1) Copy these files to a folder. And run them from within that folder.
 %
 %bayes_table.m
-%more will be added here later
+%MLE_to_BF.m
+%
+%More scripts will be added here later
 %
 %2) Run bayes_table.m to explore a classic problem in Bayesian estimation
-%3) For more info type 
+%3) Run MLE_to_BF.m to convert (log)likelihoods, obtained under MLE model estimation
+%   for two models, into Bayes factors comparing the two models.
+%
+%) For more info type 
+%   
 %   help bayes_table 
+%
+%   OR
+%
+%   help MLE_to_BF
+%
 %   at the Matlab command prompt
